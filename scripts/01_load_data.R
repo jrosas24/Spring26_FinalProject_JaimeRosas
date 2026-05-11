675a3434d7a1ca3da8c67c330f8eac985bf75e15
Jr Rosas <jrosas24@MacBookPro.lan>
2026-05-11 11:45:05 -0700

    Completed NHANES project setup

---
+++ scripts/01_load_data.R
@@ -0,0 +1,9 @@
+library(haven)
+
+demo <- read_xpt("data/P_DEMO (5).xpt")
+bmx  <- read_xpt("data/P_BMX.xpt")
+paq  <- read_xpt("data/P_PAQ.xpt")
+
+bmx <- read_xpt("data/P_BMX.xpt")
+
+demo <- read_xpt("data/P_DEMO (5).xpt")
